6c097d30af74cb80a429d236fd77676e62b30f0b
lovekarlsson18 <love.karlsson@student.kyh.se>
2020-08-31 13:46:27 +0200

    first commit

---
+++ olofuppgift1.py
@@ -0,0 +1,8 @@
+print("""
+    Hej mitt namn är Love Karlsson. Jag växte upp i Partille.
+    Jag har inga direkta kunskaper om programmering innan detta.
+    Jag sökte till KYH för att jag alltid har haft ett intresse
+    för programmering men har aldrig fått tummen ur röven och 
+    lärt mig det.
+    
+    """)

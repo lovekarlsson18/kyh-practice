3b8d3d5fc595c764216376e78e0cff95b7defd28
lovekarlsson18 <love.karlsson@student.kyh.se>
2020-09-01 10:46:54 +0200

    guessing game del 1

--- guessgame.py
+++ guessgame.py
@@ -4,22 +4,25 @@ n = random.randint(1, 100)
 print("Jag tänker på ett tal mellan 1-100. Gissa vilket?")
 gissning = 0
 
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
+def mainloop(gissning):
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
 
-    if as_number == n:
-        gissning += 1
-        print("Rätt!")
-        print("Du hade totalt: " + str(gissning) + " " "gissningar")
-        break
+        if as_number == n:
+            gissning += 1
+            print("Rätt!")
+            print("Du hade totalt: " + str(gissning) + " " "gissningar")
+            break
 
-    if as_number < n:
-        print("Fel! Mitt nummer är högre... Testa igen!")
-        gissning += 1
-        print("Antal gissningar: " + str(gissning))
+        if as_number < n:
+            print("Fel! Mitt nummer är högre... Testa igen!")
+            gissning += 1
+            print("Antal gissningar: " + str(gissning))
 
-    if as_number > n:
-        print("Fel! Mitt nummer är lägre... Testa igen!")
-        gissning += 1
-        print("Antal gissningar: " + str(gissning))
+        if as_number > n:
+            print("Fel! Mitt nummer är lägre... Testa igen!")
+            gissning += 1
+            print("Antal gissningar: " + str(gissning))
+
+mainloop(gissning)

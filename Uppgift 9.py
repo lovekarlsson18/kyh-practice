036a6cceb463bb0f8188fc43e6ffff2a2bdf5078
lovekarlsson18 <love.karlsson@student.kyh.se>
2020-09-03 09:33:20 +0200

    uppgift 9 rätta till felen

---
+++ Uppgift 9.py
@@ -0,0 +1,24 @@
+import random
+
+
+def game():
+    correct_answers = 0
+    for i in range(3):
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        answer = input(str(a) + "+" + str(b))
+        number = int(answer)
+
+        if number == a + b:
+            print("Rätt!")
+            correct_answers += 1
+
+        else:
+            print(f"Fel... Det blir {a+b}")
+            print("---")
+
+    print("Du fick " + str(correct_answers) + " av 3 rätt.")
+
+
+if __name__ == '__main__':
+    game()

1294e6a4a8e21fa7829a6f4136613a6d0735b7c6
lovekarlsson18 <love.karlsson@student.kyh.se>
2020-09-07 11:52:48 +0200

    uppgift 12.4 fixed

--- Uppgift 12.py
+++ Uppgift 12.py
@@ -2,19 +2,20 @@ def is_it_too_long(name, max_length):
     return len(name) > max_length
 
 
-def main(name):
+def main():
     #students = ["anna", "beatrice", "cecilia", "doris", "esmeralda", "frida", "gunilla"]
+    try:
+        max_length = int(input("Hur långt får namnet vara max? "))
+    except ValueError:
+        max_length = 5
+
     students = input("Skriv studenternas namn med komma imellan: ").split(",")
 
     for name in students:
+        name = name.strip()
         if is_it_too_long(name, max_length):
             print(f"{name.title()} är för långt!")
 
 
 if __name__ == '__main__':
-    try:
-        max_length = int(input("Hur långt får namnet vara max?"))
-    except ValueError:
-        name = 5
-
-    main(max_length)
+    main()

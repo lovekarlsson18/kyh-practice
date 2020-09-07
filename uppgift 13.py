e745a6c5ec384836ba2ace6246ff35cb8eeabbb0
lovekarlsson18 <love.karlsson@student.kyh.se>
2020-09-07 14:19:17 +0200

    uppgift 13

--- uppgift 13.py
+++ uppgift 13.py
@@ -8,10 +8,11 @@ def main():
     sverige = ["Narnia", "Midgård", "Norge", "Finland"]
     markku_tervahauta = ["Peter Forsberg", "Kattla", "Darth Vader", "Markoolio"]
     en_app = ["Ett dokument", "Ett hologram", "En stenmålning", "Ett datorspel"]
+    gruppen = ["Dödsätarna, DÄ", "Svenska fotbollsförbundet, SvFF", "Anonyma alkolisterna, AA", "Din mamma, DM"]
 
     print(f"""{en_app[random.randint(0,3)]} som kan laddas ned till {mobiltelefonen[random.randint(0, 3)]} ska varna finländare som kommit nära någon som smittats med {coronaviruset[random.randint(0, 3)]}.
 – Jag tycker att ni i {sverige[random.randint(0, 3)]} borde överväga att göra något liknande, säger {markku_tervahauta[random.randint(0, 3)]}, generaldirektör för
-Finska institutet för hälsa och välfärd, THL.""")
+{gruppen[random.randint(0,3)]}.""")
 
 
 if __name__ == '__main__':

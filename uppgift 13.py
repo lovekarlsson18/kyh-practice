82f481248480d50e4ddae3c7d9b832d94c8f4ab4
lovekarlsson18 <love.karlsson@student.kyh.se>
2020-09-07 15:04:19 +0200

    uppgift 13

--- uppgift 13.py
+++ uppgift 13.py
@@ -9,9 +9,10 @@ def main():
     markku_tervahauta = ["Peter Forsberg", "Kattla", "Darth Vader", "Markoolio"]
     en_app = ["Ett dokument", "Ett hologram", "En stenmålning", "Ett datorspel"]
     gruppen = ["Dödsätarna, DÄ", "Svenska fotbollsförbundet, SvFF", "Anonyma alkolisterna, AA", "Din mamma, DM"]
+    chef = ["Sith lord", "Styvfar", "Gudfadern", "Bambatant"]
 
     print(f"""{en_app[random.randint(0,3)]} som kan laddas ned till {mobiltelefonen[random.randint(0, 3)]} ska varna finländare som kommit nära någon som smittats med {coronaviruset[random.randint(0, 3)]}.
-– Jag tycker att ni i {sverige[random.randint(0, 3)]} borde överväga att göra något liknande, säger {markku_tervahauta[random.randint(0, 3)]}, generaldirektör för
+– Jag tycker att ni i {sverige[random.randint(0, 3)]} borde överväga att göra något liknande, säger {markku_tervahauta[random.randint(0, 3)]}, {chef[random.randint(0, 3)]} för
 {gruppen[random.randint(0,3)]}.""")
 
 
